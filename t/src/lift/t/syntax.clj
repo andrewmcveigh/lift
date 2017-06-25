ca0f1a2ed47e88e8079eea0debe900025d5062c3
Andrew Mcveigh <me@andrewmcveigh.com>
2017-06-25 22:42:03 +0100

    Add syntax spec/parser

---
+++ t/src/lift/t/syntax.clj
@@ -0,0 +1,74 @@
+(ns lift.t.syntax
+  (:require [clojure.spec.alpha :as s]
+            [clojure.walk :as walk]))
+
+(s/def ::literal
+  (s/or ::boolean? boolean?
+        ::char?    char?
+        ::string?  string?
+        ::integer? integer?
+        ::decimal? decimal?
+        ::keyword? keyword?))
+
+(def literal-map
+  (->> ::literal s/describe rest (apply hash-map)))
+
+(s/def ::var simple-symbol?)
+
+(s/def ::lambda
+  (s/and seq?
+         (s/cat ::lamb #{'fn}
+                ::bind (s/coll-of ::var :kind vector?)
+                ::expr ::expr)))
+
+(s/def ::application
+  (s/and seq?
+         (s/cat ::op ::expr ::args (s/* ::expr))))
+
+(s/def ::let
+  (s/and seq?
+         (s/cat ::let #{'let}
+                ::bind (s/tuple ::var ::expr)
+                ::expr ::expr)))
+
+(s/def ::if
+  (s/and seq?
+         (s/cat ::if #{'if}
+                ::cond ::expr
+                ::then ::expr
+                ::else ::expr)))
+
+(s/def ::expr
+  (s/or ::Lit ::literal
+        ::Var ::var
+        ::Lam ::lambda
+        ::App ::application
+        ::Let ::let
+        ::If  ::if))
+
+(defn normalize [[syn-type node]]
+  (case syn-type
+    ::Lit [::Lit (get literal-map (first node))]
+    ::Var [::Var node]
+    ::Lam [::Lam [(first (::bind node))
+                  (normalize (::expr node))]]
+    ::App (letfn [(app [op args]
+                    (if (seq args)
+                      (recur [::App [op (first args)]] (rest args))
+                      op))]
+            (app (normalize (::op node))
+                 (map normalize (::args node))))
+    ::Let [::Let [(first (::bind node))
+                  (normalize (second (::bind node)))
+                  (normalize (::expr node))]]
+    ::If  [::If [(normalize (::cond node))
+                 (normalize (::then node))
+                 (normalize (::else node))]]))
+
+(defn parse [expr]
+  (let [ast (s/conform ::expr expr)]
+    (if (s/invalid? ast)
+      (do
+        (s/explain ::expr expr)
+        (throw (Exception. "Invalid Syntax")))
+      (normalize ast))))
